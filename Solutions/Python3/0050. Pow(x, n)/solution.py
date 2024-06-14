b952fb48f6b33e025cde4dc18ee7f24eaca0fe37
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2024-06-13 23:39:24 -0400

    change directory names for proper structure

---
+++ Solutions/Python3/0050. Pow(x, n)/solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+
+        if n < 0:
+            x = 1/x
+            n = -n
+
+        result = 1
+        current_product = x
+        
+        while n > 0:
+            if n % 2 == 1:
+                result *= current_product
+            current_product *= current_product
+            n //= 2
+        
+        return result
