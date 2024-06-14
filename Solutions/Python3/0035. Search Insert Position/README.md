be7b218daed27d8f340f4f2a62d75653ef96eb29
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2024-06-14 09:13:45 -0400

    New solution

---
+++ Solutions/Python3/0035. Search Insert Position/README.md
@@ -0,0 +1,15 @@
+# Solution Walk Through
+Question: https://leetcode.com/problems/search-insert-position/
+
+## Intuition
+Use binary search
+
+## Approach
+- Implement a binary search algorithm
+- Instead of returning -1 if no value is found, return low, which would be the index where it would be if it were inserted in order.
+
+## Time Complexity
+$O(logn)$
+
+## Space Complexity
+$O(1)$
---
+++ Solutions/Python3/0035. Search Insert Position/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        high = len(nums) - 1
+        low = 0
+
+        while low <= high:
+            mid = (high + low) // 2
+            if target < nums[mid]:
+                high = mid - 1
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                return mid
+        return low
+        
