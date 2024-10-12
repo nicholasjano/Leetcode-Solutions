ef9b173017dc28084286e083afada5cd2c843d18
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2024-10-11 22:27:34 -0400

    Updated Solution

--- Solutions/Python3/518. Coin Change II/solution.py
+++ Solutions/Python3/518. Coin Change II/solution.py
@@ -1,10 +1,10 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
-            return n
-        
-        f1, f2 = 0, 1
-        for _ in range(n-1):
-            f1, f2 = f2, f1 + f2
+class Solution:    
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
 
-        return f2
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+
+        return dp[-1]
