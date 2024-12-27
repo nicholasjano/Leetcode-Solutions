df16de91f16623e91d81f7c1ba38665403ee7f08
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2024-12-26 19:37:56 -0500

    Fixed Solution

--- Solutions/Python3/0670. Maximum Swap/solution.py
+++ Solutions/Python3/0670. Maximum Swap/solution.py
@@ -1,27 +1,34 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        carry_over = 0
-        current = ListNode()
-        head = current
+    def maximumSwap(self, num: int) -> int:
+        digits = []
+        while num > 0:
+            digits.append(num % 10)
+            num //= 10
+        
+        digits = digits[::-1]
+        
+        swap_needed = False
+        start_index = -1
+        for i in range(len(digits) - 1):
+            if digits[i] < digits[i+1]:
+                swap_needed = True
+                start_index = i
+                break
+        
+        if swap_needed:
+            highest_index = start_index
+            for i in range(start_index + 1, len(digits)):
+                if digits[i] >= digits[highest_index]:
+                    highest_index = i
+            
+            for i in range(start_index + 1):
+                if digits[i] < digits[highest_index]:
+                    digits[i], digits[highest_index] = digits[highest_index], digits[i]
+                    break
 
-        while l1 or l2:
-            if l1:
-                current.val += l1.val
-            if l2:
-                current.val += l2.val
-
-            carry_over = current.val // 10
-            current.val -= 10 * carry_over
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            if l1 or l2 or carry_over > 0:
-                current.next = ListNode()
-                current = current.next
-                current.val = carry_over
-
-        return head
+        
+        final_num = 0
+        for digit in digits:
+            final_num = final_num * 10 + digit
+        
+        return final_num
