2b25ff1252d49d67444008ccc954628e559d49b0
nicholasjano <85026552+nicholasjano@users.noreply.github.com>
2025-04-03 16:21:03 -0400

    New Solution

--- Solutions/Python3/0054. Spiral Matrix/README.md
+++ Solutions/Python3/0054. Spiral Matrix/README.md
@@ -2,12 +2,15 @@
 Question: https://leetcode.com/problems/spiral-matrix/
 
 ## Intuition
-Create an extra list to store the spiral-ordered values. Have a flag that holds the current moving direction (right, left, down, up). Once a barrier in one direction has been reached, I will update the flag with the new direction. There will be counters to shorten the barrier as the spiral goes on, as the navigation will need shorter movements per direction as it approches the center.
+I can create pointers storing the rows/columns that have yet to be iterated through, as well as a variable to store the current direction. Starting right, add each element from the top row, then move the top pointer down one, and so on until the pointers overlap.
 
 ## Approach
-- Declare inital variables that will be used in the loop.
-- In each iteration of the loop, update the current position on the final list.
-- Run an if statement to check the current direction. If the next move is in the same direction, update the current position to the next position in that direction. Otherwise, update the moving direction flag, and move in the new direction.
+- Initialize `top`, `bottom`, `left`, `right` variables for the current bounds.
+- Create a while loop that will end once the pointers surpass eachother.
+- Start navigating from `left` to `right` inclusive, adding each value to the result list. Afterward, increment `top` as the top row has been accounted for.
+- Navigate down from `top` to `bottom` inclusive, adding each value to the result list. Afterward, decrement `right` as the right column has been accounted for.
+- Continue this for left, up, and repeat if needed.
+- Return the result list once every value has been accounted for.
 
 ## Time Complexity
 $O(n \cdot m)$
--- Solutions/Python3/0054. Spiral Matrix/solution.py
+++ Solutions/Python3/0054. Spiral Matrix/solution.py
@@ -1,38 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        n = len(matrix)
-        m = len(matrix[0])
-        curr = [0, 0]
-        curr_direction = 0 # 0 = right, 1 = left, 2 = down, 3 = up
-        final = [0] * (n*m)
-        up_down_counter = 0
-        left_right_counter = 0
-        for i in range(n*m):
-            final[i] = matrix[curr[0]][curr[1]]
-            if curr_direction == 0:
-                if (curr[1] + 1) == (m - left_right_counter):
-                    curr_direction = 2
-                    curr[0] += 1
-                else:
-                    curr[1] += 1
-            elif curr_direction == 1:
-                if curr[1] == (0 + left_right_counter):
-                    curr_direction = 3
-                    curr[0] -= 1
-                    left_right_counter += 1
-                else:
-                    curr[1] -= 1
-            elif curr_direction == 2:
-                if (curr[0] + 1) == (n - up_down_counter):
-                    curr_direction = 1
-                    curr[1] -= 1
-                    up_down_counter += 1
-                else:
-                    curr[0] += 1
-            elif curr_direction == 3:
-                if curr[0] == (0 + up_down_counter):
-                    curr_direction = 0
-                    curr[1] += 1
-                else:
-                    curr[0] -= 1
-        return final
+        if not matrix:
+            return []
+
+        m, n = len(matrix), len(matrix[0])
+        result = []
+        top, bottom = 0, m - 1
+        left, right = 0, n - 1
+        direction = 0 # 0 = right, 1 = down, 2 = left, 3 = up
+
+        while top <= bottom and left <= right:
+            if direction == 0:
+                for i in range(left, right + 1):
+                    result.append(matrix[top][i])
+                top += 1
+            elif direction == 1:
+                for i in range(top, bottom + 1):
+                    result.append(matrix[i][right])
+                right -= 1
+            elif direction == 2:
+                for i in range(right, left - 1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+            elif direction == 3:
+                for i in range(bottom, top - 1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+            direction = (direction + 1) % 4
+
+        return result
